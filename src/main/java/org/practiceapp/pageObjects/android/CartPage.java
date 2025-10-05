1304cf66cbfd2e6d488832ed3784c592a626fd64
Dhananjay <dhananjayparit22@gmail.com>
2025-10-05 22:22:22 +0530

    deleted duplicate method

--- src/main/java/org/practiceapp/pageObjects/android/CartPage.java
+++ src/main/java/org/practiceapp/pageObjects/android/CartPage.java
@@ -59,17 +59,6 @@ public class CartPage extends AndroidActions {
 		return amount;
 	}
 	
-	public double getProductSum2()
-	{
-		double amount=0;
-		for(int i=0; i<displayPrices.size(); i++)
-		{
-			String price = displayPrices.get(i).getText();
-			double formattedPrice = getFormattedAmount(price);
-			amount = amount + formattedPrice;
-		}
-		return amount;
-	}
 	
 	public double getTotalAmountDisplayed()
 	{
